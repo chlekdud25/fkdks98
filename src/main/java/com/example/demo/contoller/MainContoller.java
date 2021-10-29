f97137b30c9d1ba573f38570a3229fad3be55405
chlekdud25 <fkdks98@naver.com>
2021-10-29 15:43:21 +0900

    테스트입니다

---
+++ src/main/java/com/example/demo/contoller/MainContoller.java
@@ -0,0 +1,30 @@
+package com.example.demo.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.BoardDTO;
+
+@RestController
+public class MainContoller {
+
+	@Autowired
+	BoardDTO dto;
+	
+	@RequestMapping(value="/")
+	public String main() {
+		return "index";
+	}
+	
+	@RequestMapping("/text1")
+	public BoardDTO test1() {
+		
+		
+		dto.setTitle("테스트1");
+		dto.setContent("테스트입니다");
+		return dto;
+	}
+}
